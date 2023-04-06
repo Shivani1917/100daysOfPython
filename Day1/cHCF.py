0fd11a67adbb80d2f01da71ccc44a359988a95ee
Shivani Badola <79411680+Shivani1917@users.noreply.github.com>
2023-04-06 19:01:35 +0530

    Add files via upload

---
+++ Day1/cHCF.py
@@ -0,0 +1,14 @@
+# Program to display the HCF of two numbers
+
+def findHCF(num1,num2):
+    if num1 > num2:
+        smaller = num2
+    else:
+        smaller = num1
+    for i in range(1,smaller + 1):
+        if ((num1%i==0) and (num2%i==0)):
+           hcf = i
+
+    return hcf
+
+print(findHCF(6,12))
