c036e61dc303b53545f82165c76826fc2dd76592
Shivani Badola <79411680+Shivani1917@users.noreply.github.com>
2023-04-14 20:21:01 +0530

    Update aPrintDuplicates.py

--- Day8/aPrintDuplicates.py
+++ Day8/aPrintDuplicates.py
@@ -1,25 +1,25 @@
-#  Program for Increamenting Numeric String
+#  Program for Incrementing Numeric String
 
-a = []
-n = int(input("Enter number of elements: "))
+def increment_numeric_string(input_str):
+    if not input_str.isdigit():
+        raise ValueError("Input must be a numeric string")
 
-print("Enter elements of the list")
-for i in range(0, n):
-    element = int(input())
-    a.append(element)
+    num = int(input_str)
 
-print(a)
+    num += n
 
-n1 = int(input("Increament of "))
+    result_str = str(num)
 
-def Increment_Numeric_String(Test_list, n1):
-    Result = []
-    for ele in Test_list:
-        if ele.isDigit():
-            Result.append(str(int(ele)+n1))
-        else:
-            Result.append(ele)
-    return Result
+    return result_str
 
-print(Increment_Numeric_String(a, n1))
+input_str = input("Enter a numeric string: ")
+n = int(input("Enter the increment number: "))
+try:
+    result_str = increment_numeric_string(input_str)
+    print("Incremented result:", result_str)
+except ValueError as e:
+    print("Error:", e)
+
+
+
 
